74ad62257c7d5ff04f8ad466003f4b1e0cdd0a96
Rogers <mohammedrogers579@gmail.com>
2025-10-27 10:43:28 -0700

    feat(UI): add footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -30,6 +31,7 @@ export default function RootLayout({
         <main className="font-sans antialiased bg-neutral-950 text-white">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+    return (            
+        <footer className="bg-neutral-950 text-white">
+            <div className="container mx-auto text-center">
+                <p>&copy; {new Date().getFullYear()} Built with 💗 by Rogers</p>                       
+            </div>
+        </footer>        
+    );
+}
